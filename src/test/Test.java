fc4ae72deacae396c0ce0298c45a01d3239c9edd
mushtaq93 <mushtaq93@gmail.com>
2017-05-23 23:52:17 +0530

    x=2

--- src/test/Test.java
+++ src/test/Test.java
@@ -15,6 +15,8 @@ public class Test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int x =2;
+        
         // TODO code application logic here
     }
     
